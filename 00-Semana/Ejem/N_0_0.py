d7194bdae70ff11de4fc9a194b92ff31b951020c
chon-94 <107455342+chon-94@users.noreply.github.com>
2023-08-02 09:47:13 -0500

    neurona

---
+++ 00-Semana/Ejem/N_0_0.py
@@ -0,0 +1,11 @@
+# pylint: disable=unused-import
+import numpy as np                # Matrices y Funciones Matematicas 
+import matplotlib.pyplot as plt   # Gragicar y plotear
+import tensorflow as tf           # Redes Neuronales             
+import pandas as pd               # Analisis y manejo de datos
+import sklearn  as skl         # modelos
+   
+a = 2
+b = 3
+
+print(a+b)
