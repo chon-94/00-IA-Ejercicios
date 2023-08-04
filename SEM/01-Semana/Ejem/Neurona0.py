4190494ef002f10223587896605d3d69cf4ce091
chon-94 <107455342+chon-94@users.noreply.github.com>
2023-08-04 12:39:06 -0500

    neurona

--- SEM/01-Semana/Ejem/Neurona0.py
+++ SEM/01-Semana/Ejem/Neurona0.py
@@ -1,17 +1,17 @@
 import numpy as np                    #matrices y funciones matematicas
 import matplotlib.pyplot as plt       #gráficos y ploteos
 import tensorflow as tf               #redes neuronales
-#Modelo de una neurona artificial
 from tensorflow import keras
 from tensorflow.keras import Sequential,layers
 
-x=np.array([2,3,4,5,6,7,8,9])
+# Datos de entrenamiento
+x=np.array([2  ,3  ,4  ,5  ,6  ,7  ,8  ,9])
 y=np.array([2.1,3.2,4.1,4.9,6.2,7.1,7.8,9.1] )
 
-modelo=Sequential([layers.Dense(units=1,input_shape=[1],activation=None)])
+modelo=Sequential([layers.Dense(units=9,input_shape=[7],activation=None)])
 modelo.compile(optimizer="sgd",loss="mean_squared_error")
 
-hist=modelo.fit(x,y,epochs=100)
+hist=modelo.fit(x,y,epochs=5)
 
 loss_mse=hist.history["loss"]
 epochs=range(1,len(loss_mse)+1)
